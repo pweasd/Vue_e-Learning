72271ea14bf8b1a9659d87785a6cb58b792c586e
Lee Sue Hyun <hyunls33@dalsoft.co.kr>
2020-05-03 19:20:35 +0900

    오시는길 작업
    오시는길 작업

--- code/hotel/src/views/Location.vue
+++ code/hotel/src/views/Location.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="location">
-    <div id="map" style="width:100%;height:400px;"></div>
+    <p class="location_title">오시는 길</p>
+
+    <div id="map"></div>
+
+    <p>충청남도 천안시 동남구 병천면 충절로 1600</p>
   </div>
 </template>
 
@@ -8,15 +12,15 @@
   export default {
     mounted() {
       var mapOptions = {
-          center: new naver.maps.LatLng(36.7647, 127.2824),
-          zoom: 17
+        center: new naver.maps.LatLng(36.7647, 127.2824),
+        zoom: 17
       };
 
       var map = new naver.maps.Map('map', mapOptions);
 
       var marker = new naver.maps.Marker({
-          map: map,
-          position: mapOptions.center
+        map: map,
+        position: mapOptions.center
       });
     },
   }
@@ -26,6 +30,19 @@
 .location {
   width: 100%;
   height: 100%;
+  padding: 80px 0px;
   background: #EFF0F2;
+  box-sizing: border-box;
+  color: #343434;
+  text-align: center;
+  .location_title {
+    font-weight: bold;
+    font-size: 36px;
+  }
+  #map {
+    width:1180px;
+    height:640px;
+    margin: 50px auto 28px auto;
+  }
 }
 </style>
