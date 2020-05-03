5fe8a4e8d7f3aef5c080df72f91ace5ead51491a
KimKyuTae7030 <38874974+KimKyuTae7030@users.noreply.github.com>
2020-05-03 12:13:06 +0900

    skills작업

--- code/portfolio/src/App.vue
+++ code/portfolio/src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Skills></Skills>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+  import Skills from './components/Skills.vue'
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Skills,
+    },
   }
-};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
