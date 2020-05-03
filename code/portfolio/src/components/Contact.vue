661b0f88a4c4e9b344001c01f0f1554dd403a1b0
sangyoon <sangyoon@dalsoft.co.kr>
2020-05-03 12:20:20 +0900

    About, Contact

---
+++ code/portfolio/src/components/Contact.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="contact_container">
+    <p class="contact_Title">contact</p>
+    <div class="contact_Line"></div>
+
+    <div class="contact_EmailImage">
+      <img src="@/assets/img_email.png" />
+    </div>
+
+    <div class="contact_Area">
+      <p>SEOUL, SOUTH KOREA</p>
+    </div>
+
+    <div class="contact_Email">
+      <p>Honggildong@portfolio.co.kr / 010-1234-5678</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  imagePath: '',
+}
+</script>
+
+<style lang="scss" scoped>
+.contact_container {
+  width: 100%;
+  height: 100%;
+
+  .contact_Title {
+    margin: 92px 0px 0px 240px;
+    width: 292px;
+    height: 107px;
+    font-size: 72px;
+    font-style: bold;
+    text-align: left;
+    letter-spacing: 2.16px;
+    color: #111111;
+  }
+
+  .contact_Line {
+    margin: 6px 0px 0px 240px;
+    width: 147px;
+    height: 6px;
+    background: #1a1a1a 0% 0% no-repeat padding-box;
+  }
+
+  .contact_EmailImage {
+    width: 60px;
+    height: 47px;
+    margin: 70px 0px 0px 240px;
+  }
+
+  .contact_Area {
+    width: 370px;
+    height: 47px;
+    margin: 25px 0px 0px 240px;
+    font-size: 32px;
+    text-align: left;
+    color: #232323;
+  }
+
+  .contact_Email {
+    width: 500px;
+    height: 36px;
+    margin: 14px 0px 0px 240px;
+    font-size: 24px;
+    text-align: left;
+    color: #232323;
+  }
+}
+</style>
---
+++ code/portfolio/src/components/PageAbout.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="page_Container">
+      <p class="page_About">About</p>
+      <div class="page_Line"></div>
+
+      <div class="page_Purpose">
+        <p>
+          어필하고 싶은 자신의 장점과<br />
+          앞으로 해내고 싶은 목표에 대해 작성.
+        </p>
+      </div>
+
+      <div class="page_Detail">
+        <p>좀 더 자세하고 세부적이며 길게 늘여 쓸만한 자신의 이야기를 작성.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.page_Container {
+  width: 100%;
+  height: 100%;
+
+  .page_About {
+    width: 222px;
+    height: 107px;
+    margin: 92px 0px 0px 240px;
+    text-align: left;
+    font-size: 72px;
+    font-style: bold;
+    letter-spacing: 2.16px;
+    color: #111111;
+  }
+
+  .page_Line {
+    margin: 6px 0px 0px 240px;
+    width: 147px;
+    height: 6px;
+    background: #1a1a1a 0% 0% no-repeat padding-box;
+  }
+
+  .page_Purpose {
+    margin: 88px 0px 0px 240px;
+    width: 550px;
+    height: 99px;
+    font-size: 32px;
+    text-align: left;
+    color: #232323;
+  }
+
+  .page_Detail {
+    margin: 43px 0px 0px 240px;
+    width: 720px;
+    height: 36px;
+    font-size: 24px;
+    text-align: left;
+    color: #232323;
+  }
+}
+</style>
