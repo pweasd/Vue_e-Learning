901927138244456ad3ad20f62da281bf2c18cff9
Sanghyeon <sanghyeon@dalsoft.co.kr>
2020-05-03 11:21:10 +0900

    포트폴리오 프로젝트 수정
    포트폴리오 프로젝트 수정

--- code/portfolio/src/App.vue
+++ code/portfolio/src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
