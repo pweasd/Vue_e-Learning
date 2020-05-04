96b82f8751e94d0b474e4627b9cf285b9b0e42a2
Lee Sue Hyun <hyunls33@dalsoft.co.kr>
2020-05-04 12:37:00 +0900

    localStorage
    localStorage

--- code/hotel/src/store/index.js
+++ code/hotel/src/store/index.js
@@ -1,22 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
-// library
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-// el-element 언어 설정
-import Lang from 'element-ui/lib/locale/lang/ko'
-import Locale from 'element-ui/lib/locale'
-
-Locale.use(Lang)
+import VueLocalStorage from 'vue-localstorage'
 
 Vue.use(Vuex);
-Vue.use(ElementUI)
+Vue.use(VueLocalStorage, {
+  name: 'ls',
+  bind: true
+})
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    customerList: [],
+    reservationStateList: []
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {} 
 });
