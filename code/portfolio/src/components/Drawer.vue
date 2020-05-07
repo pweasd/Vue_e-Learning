20a487406763f08656f1b292fc2128ceb9c361d1
sangyoon <sangyoon@dalsoft.co.kr>
2020-05-07 14:53:04 +0900

    drawer 수정
    drawer 레이아웃 수정

--- code/portfolio/src/components/Drawer.vue
+++ code/portfolio/src/components/Drawer.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="drawer_container" v-show="openDrawer">
-    <div class="drawer_background" :class="open ? 'fade_in' : 'fade_out'" @click.self="close"/>
+    <div class="drawer_background" :class="open ? 'fade_in' : 'fade_out'" @click.self="close" />
     <div class="drawer_content" :class="open ? 'drawer_in' : 'drawer_out'">
       <ul>
-        <li v-for="menu in menuList" :key="menu.id">{{ menu }}</li>
+        <li v-for="menu in menuList" :key="menu.id" @click="move(menu)">{{ menu }}</li>
       </ul>
-      <p>SEOUL, SOUTH KOREA</p>
-      <p>이메일@입력하세요.</p>
-      <p>010-번호를-입력하세요.</p>
+      <div class="text">
+        <img src="../assets/img_git.png" class="image" />
+        <p class="text_location">SEOUL, SOUTH KOREA</p>
+        <p class="text_contact">Honggildong@portfolio.co.kr</p>
+        <p class="text_contact">010-1234-5678</p>
+      </div>
     </div>
   </div>
 </template>
@@ -16,31 +19,39 @@
 export default {
   props: {
     menuList: {
-      default: Array
+      default: Array,
     },
     open: {
-      type: Boolean
-    }
+      type: Boolean,
+    },
+  },
+  mounted() {
+    window.addEventListener('resize', () => {
+      this.$emit('close')
+    })
   },
   data() {
-    return {      
+    return {
       openDrawer: false,
     }
   },
   methods: {
     close() {
       this.$emit('close')
-    }
+    },
+    move(menu) {
+      this.$emit('move', menu)
+    },
   },
   watch: {
     open(value) {
       if (value) this.openDrawer = value
-      else window.setTimeout(() => {
-        this.openDrawer = value
-      }, 300)
-    }
-  }
-
+      else
+        window.setTimeout(() => {
+          this.openDrawer = value
+        }, 300)
+    },
+  },
 }
 </script>
 
@@ -52,7 +63,8 @@ export default {
   width: 100%;
   height: 100%;
   overflow: hidden;
-  
+  z-index: 1;
+
   .drawer_background {
     width: 100%;
     height: 100%;
@@ -93,32 +105,58 @@ export default {
     top: 0;
     bottom: 0;
     width: 60%;
-    // height: 100%;
     box-sizing: border-box;
     background-color: #fff;
     display: flex;
     flex-direction: column;
-    box-shadow: 0 8px 10px -5px rgba(0,0,0,.2), 0 16px 24px 2px rgba(0,0,0,.14), 0 6px 30px 5px rgba(0,0,0,.12);
+    box-shadow: 0 8px 10px -5px rgba(0, 0, 0, 0.2), 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12);
     overflow: hidden;
     background-color: #000;
 
     ul {
-      color: #EEEEEE;
+      color: #eeeeee;
       font-size: 30px;
+      box-sizing: border-box;
       font-weight: bold;
+      padding-top: 100px;
+      padding-bottom: 170px;
       li {
         opacity: 0.5;
         transition: 0.2s;
+        margin-bottom: 15px;
+        letter-spacing: 2.5px;
       }
       li:hover {
         opacity: 1;
         transition: 0.2s;
+        cursor: pointer;
+      }
+    }
+
+    .text {
+      position: relative;
+      color: #eeeeee;
+      box-sizing: border-box;
+
+      .image {
+        width: 50px;
+        margin-bottom: 10px;
+      }
+      .text_location {
+        font-size: 14px;
+        margin-bottom: 10px;
+      }
+      .text_contact {
+        font-size: 12px;
+        margin-bottom: 5px;
       }
     }
   }
 
   .drawer_in {
     animation: drawer_in 0.3s;
+    box-sizing: border-box;
+    padding-left: 10%;
   }
 
   .drawer_out {
--- code/portfolio/src/components/Home.vue
+++ code/portfolio/src/components/Home.vue
@@ -11,174 +11,174 @@
     </div>
 
     <span class="author">Made by Hong</span>
-    <button class="menu_icon" :class="open ? 'active' : ''" @click="open ? open = false : open = true">
+    <button class="menu_icon" :class="open ? 'active' : ''" @click="open ? (open = false) : (open = true)">
       <div class="line line1"></div>
       <div class="line line2"></div>
       <div class="line line3"></div>
     </button>
-    <Drawer :menuList="menuList" :open="open" @close="open = false"></Drawer>
+    <Drawer :menuList="menuList" :open="open" @close="open = false" @move="move"></Drawer>
   </div>
 </template>
 
 <script>
 import Drawer from './Drawer'
 
-  export default {
-    components: {
-      Drawer
+export default {
+  components: {
+    Drawer,
+  },
+  data() {
+    return {
+      menuList: ['Home', 'About', 'Skills', 'Project', 'Contact'],
+      open: false,
+    }
+  },
+  methods: {
+    move(menu) {
+      this.$emit('move', menu)
     },
-    data() {
-      return {
-        menuList: [ 'Home', 'About', 'Skills', 'Project', 'Contact' ],
-        open: false
-      }
-    },
-    methods: {
-      move(menu) {
-        this.$emit('move', menu)
-      },
-    },
-  }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-  @media (min-width: 801px) {
-    .home_container {
-      position: relative;
-      width: 100%;
-      height: 100%;
-      background-image: url('../assets/img_bg.png');
-      background-repeat: no-repeat;
-      background-size: cover;
+@media (min-width: 801px) {
+  .home_container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-image: url('../assets/img_bg.png');
+    background-repeat: no-repeat;
+    background-size: cover;
 
-      .home_nav {
-        display: flex;
-        justify-content: flex-end;
-        box-sizing: border-box;
-        padding-top: 30px;
-        padding-right: 180px;
-        button {
-          width: 80px;
-          height: 30px;
-          background-color: transparent;
-          font-size: 18px;
-          color: var(--title-color);
-          &:hover {
-            color: #bbbbbb;
-          }
-        }
-      }
-
-      .home_title {
-        position: absolute;
-        top: 60%;
-        left: 200px;
+    .home_nav {
+      display: flex;
+      justify-content: flex-end;
+      box-sizing: border-box;
+      padding-top: 30px;
+      padding-right: 180px;
+      button {
+        width: 80px;
+        height: 30px;
+        background-color: transparent;
+        font-size: 18px;
         color: var(--title-color);
-
-        .text {
-          font-size: 32px;
+        &:hover {
+          color: #bbbbbb;
         }
-
-        .text_bold {
-          font-size: 70px;
-          font-weight: 500;
-        }
-      }
-
-      .author {
-        position: absolute;
-        left: 85%;
-        top: 40%;
-        color: var(--title-color);
-        font-size: 24px;
-        transform: rotate(0.25turn);
       }
     }
-    
-    .menu_icon {
+
+    .home_title {
+      position: absolute;
+      top: 60%;
+      left: 200px;
+      color: var(--title-color);
+
+      .text {
+        font-size: 32px;
+      }
+
+      .text_bold {
+        font-size: 70px;
+        font-weight: 500;
+      }
+    }
+
+    .author {
+      position: absolute;
+      left: 85%;
+      top: 40%;
+      color: var(--title-color);
+      font-size: 24px;
+      transform: rotate(0.25turn);
+    }
+  }
+
+  .menu_icon {
+    display: none;
+  }
+}
+@media (max-width: 800px) {
+  .home_container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-image: url('../assets/img_bg.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+
+    .home_nav {
       display: none;
     }
-  }
-  @media (max-width: 800px) {
-    .home_container {
-      position: relative;
-      width: 100%;
-      height: 100%;
-      background-image: url('../assets/img_bg.png');
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position: center;
 
-      .home_nav {
-        display: none;
+    .home_title {
+      position: absolute;
+      top: 60%;
+      left: 8%;
+      color: var(--title-color);
+
+      .text {
+        font-size: 24px;
       }
 
-      .home_title {
-        position: absolute;
-        top: 60%;
-        left: 8%;
-        color: var(--title-color);
-
-        .text {
-          font-size: 24px;
-        }
-
-        .text_bold {
-          margin-top: 25px;
-          font-size: 36px;
-          font-weight: 500;
-        }
+      .text_bold {
+        margin-top: 25px;
+        font-size: 36px;
+        font-weight: 500;
       }
+    }
 
-      .author {
-        position: absolute;
-        left: 80%;
-        top: 20%;
-        color: var(--title-color);
-        font-size: 14px;
-        transform: rotate(0.25turn);
-      }
+    .author {
+      position: absolute;
+      left: 80%;
+      top: 20%;
+      color: var(--title-color);
+      font-size: 14px;
+      transform: rotate(0.25turn);
+    }
 
-      .menu_icon {
-        position: absolute;
-        left: 86%;
-        top: 3%;
-        display: block;
-        width: 20px;
-        height: 20px;
-        background-color: transparent;
-        z-index: 2;
-    
-        .line {
-          width: 100%;
-          height: 2px;
-          background-color: #fff;
-          margin-bottom: 8px;
-          transition: 0.3s;
-          &:last-child {
-            margin-bottom: 0px;
-          }
-        }
-      }
-    
-      .active {
-        .line {
-          margin: 0;
-        }
-        .line1 {
-          width: 24px;
-          // transform: translateX(-4px) rotate(45deg) translateY(-8px);
-          transform: translateX(-5px) rotate(45deg) translateY(0px);
-        }
-        .line2 {
-          width: 24px;
-          // transform: translateX(-8px) rotate(-45deg) translateY(14px);
-          transform: translateX(-3px) rotate(-45deg) translateY(-3px);
-        }
-        .line3 {
-          opacity: 0;
+    .menu_icon {
+      position: fixed;
+      left: 86%;
+      top: 3%;
+      display: block;
+      width: 20px;
+      height: 20px;
+      background-color: transparent;
+      z-index: 2;
+
+      .line {
+        width: 100%;
+        height: 2px;
+        background-color: #fff;
+        margin-bottom: 8px;
+        transition: 0.3s;
+        &:last-child {
+          margin-bottom: 0px;
         }
       }
     }
+
+    .active {
+      .line {
+        margin: 0;
+      }
+      .line1 {
+        width: 24px;
+        // transform: translateX(-4px) rotate(45deg) translateY(-8px);
+        transform: translateX(-5px) rotate(45deg) translateY(0px);
+      }
+      .line2 {
+        width: 24px;
+        // transform: translateX(-8px) rotate(-45deg) translateY(14px);
+        transform: translateX(-3px) rotate(-45deg) translateY(-3px);
+      }
+      .line3 {
+        opacity: 0;
+      }
+    }
   }
+}
 </style>
