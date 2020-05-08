5f5dead5a59c3922866b215e87dfec9759df3579
KimKyuTae7030 <38874974+KimKyuTae7030@users.noreply.github.com>
2020-05-08 17:33:17 +0900

    drawer제거

--- code/portfolio/src/App.vue
+++ code/portfolio/src/App.vue
@@ -5,60 +5,65 @@
     <Skills ref="skills"></Skills>
     <Project ref="project"></Project>
     <Contact ref="contact"></Contact>
-    <!-- <Drawer></Drawer> -->
   </div>
 </template>
 
 <script>
-import Home from './components/Home'
-import PageAbout from './components/PageAbout'
-import Skills from './components/Skills'
-import Project from './components/Project'
-import Contact from './components/Contact'
-import Drawer from './components/Drawer'
+  import Home from './components/Home'
+  import PageAbout from './components/PageAbout'
+  import Skills from './components/Skills'
+  import Project from './components/Project'
+  import Contact from './components/Contact'
 
-export default {
-  name: "App",
-  components: {
-    Home,
-    PageAbout,
-    Skills,
-    Project,
-    Contact,
-    Drawer
-  },
-  methods: {
-    move(menu) {
-      let pos;
+  export default {
+    name: 'App',
+    components: {
+      Home,
+      PageAbout,
+      Skills,
+      Project,
+      Contact,
+    },
+    methods: {
+      move(menu) {
+        let pos
 
-      switch (menu) {
-        case 'Home':
-          pos = 0;
-          break;
-        case 'About':
-          pos = this.$refs.home.$el.clientHeight
-          break
-        case 'Skills':
-          pos = this.$refs.home.$el.clientHeight + this.$refs.about.$el.clientHeight
-          break
-        case 'Project':
-          pos = this.$refs.home.$el.clientHeight + this.$refs.about.$el.clientHeight + this.$refs.skills.$el.clientHeight
-          break
-        case 'Contact':
-          pos = this.$refs.home.$el.clientHeight + this.$refs.about.$el.clientHeight + this.$refs.skills.$el.clientHeight + this.$refs.project.$el.clientHeight
-          break
-      }
+        switch (menu) {
+          case 'Home':
+            pos = 0
+            break
+          case 'About':
+            pos = this.$refs.home.$el.clientHeight
+            break
+          case 'Skills':
+            pos =
+              this.$refs.home.$el.clientHeight + this.$refs.about.$el.clientHeight
+            break
+          case 'Project':
+            pos =
+              this.$refs.home.$el.clientHeight +
+              this.$refs.about.$el.clientHeight +
+              this.$refs.skills.$el.clientHeight
+            break
+          case 'Contact':
+            pos =
+              this.$refs.home.$el.clientHeight +
+              this.$refs.about.$el.clientHeight +
+              this.$refs.skills.$el.clientHeight +
+              this.$refs.project.$el.clientHeight
+            break
+        }
 
-      window.scroll({ top: pos, left: 0, behavior: 'smooth' })
-    }
+        window.scroll({ top: pos, left: 0, behavior: 'smooth' })
+      },
+    },
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  width: 100%;
-  height: 100%;
-  font-family: 'Spoqa Han Sans Regular';
-}
+  #app {
+    width: 100%;
+    height: 100%;
+    font-family: 'Spoqa Han Sans Regular';
+  }
 </style>
